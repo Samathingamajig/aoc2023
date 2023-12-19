38a3207f0a226c302e264db55d3974539701b59f
Samuel Gunter <samathingamajig@gmail.com>
2023-12-18 22:59:04 -0600

    day 18 done

--- 18/b.ts
+++ 18/b.ts
@@ -8,26 +8,21 @@ export const solution = (input: string) => {
     return ["RDLU"[+care[5]], parseInt(care.slice(0, 5), 16)] as const;
   });
 
-  const START = 1;
-  const STOP = 2;
-  const EITHER = START | STOP;
-
   let row = 0;
   let col = 0;
-  let prevHor = "";
   // let miscounted = 0;
-  const changes: { startCol: number; endCol: number; row: number; shouldChange: number }[] = [];
 
   let maxRow = 0;
   let minRow = 0;
   let maxCol = 0;
   let minCol = 0;
 
+  let internalArea = 0;
+
   for (let di = 0; di < data.length; di++) {
     const d = data[di];
-    // for (let i = 0; i < d[1]; i++) {
     const prevCol = col;
-    let signDir = 0;
+    const prevRow = row;
     switch (d[0]) {
       case "U":
         row -= d[1];
@@ -37,40 +32,13 @@ export const solution = (input: string) => {
         break;
       case "L":
         col -= d[1];
-        signDir = -1;
         break;
       case "R":
         col += d[1];
-        signDir = 1;
         break;
     }
 
-    if (d[0] == "L" || d[0] == "R") {
-      // if (prevHor == d[0]) {
-      //   changes.push({
-      //     startCol: Math.min(prevCol, col),
-      //     endCol: Math.max(prevCol, col),
-      //     // startCol: Math.min(prevCol + signDir, col),
-      //     // endCol: Math.max(prevCol + signDir, col),
-      //     row,
-      //     shouldChange: fal se,
-      //   });
-      // } else {
-      //   changes.push({ startCol: Math.min(prevCol, col), endCol: Math.max(prevCol, col), row, shouldChange: true });
-      //   prevHor = d[0];
-      // }
-      changes.push({
-        startCol: Math.min(prevCol + signDir, col - signDir),
-        endCol: Math.max(prevCol + signDir, col - signDir),
-        row,
-        shouldChange: EITHER,
-      });
-
-      changes.push({ startCol: col, endCol: col, row, shouldChange: data[di + 1][0] == "U" ? START : STOP });
-      if (di > 0) {
-        changes.push({ startCol: col, endCol: col, row, shouldChange: data[di - 1][0] == "U" ? START : STOP });
-      }
-    }
+    internalArea += (prevCol * row - col * prevRow) / 2;
 
     maxRow = Math.max(maxRow, row);
     minRow = Math.min(minRow, row);
@@ -81,6 +49,7 @@ export const solution = (input: string) => {
   }
 
   // console.log(data);
+  // console.log(internalArea);
   console.log({ minRow, maxRow, minCol, maxCol });
   // console.log(changes);
 
@@ -118,55 +87,14 @@ export const solution = (input: string) => {
   };
   */
 
-  changes.sort((a, b) => a.row - b.row);
   // console.log(changes);
-  let area = 0;
-  console.time("foo");
-  for (let c = minCol - 1; c <= maxCol + 1; c++) {
-    let init = false;
-    let prev: (typeof changes)[number] = {
-      startCol: -Infinity,
-      endCol: Infinity,
-      row: minRow - 100,
-      shouldChange: 0,
-    };
-    // let prevRow = minRow - 100;
-    for (const change of changes) {
-      if (prev.row == change.row) continue;
-      // if (change.shouldChange == STOP) continue;
-      if (c >= change.startCol && c <= change.endCol) {
-        // if (prev.endCol == change.startCol) {
-        //   area += change.row - prev.row;
-        //   if (change.shouldChange || prev.shouldChange) {
-        //     init = !init;
-        //   }
-        // } else {
-        //   if (init) {
-        //     area += change.row - prev.row + 1;
-        //   }
 
-        //   init = !init;
-        // }
-        // prev = change;
-        if (init) {
-          // area += change.row - prev.row + 1;
-          area += change.row - prev.row - 1;
-        }
-        if (change.shouldChange == START) {
-          init = true;
-        } else if (change.shouldChange == STOP) {
-          init = false;
-        } else {
-          init = !init;
-        }
-        prev = change;
-      }
-    }
-  }
-
+  let foo = 0;
   for (const d of data) {
-    area += d[1] - 1;
+    foo += d[1];
+    // area += d[1] - 1;
   }
+  console.log({ foo });
 
   console.timeEnd("foo");
 
@@ -179,5 +107,5 @@ export const solution = (input: string) => {
 
   // return "62";
   // return 952408144115;
-  return area;
+  return internalArea + 1 - Math.floor(foo / 2) + foo;
 };
