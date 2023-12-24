34db34820643528bc67e655b57db5d93e78f8362
Samuel Gunter <samathingamajig@gmail.com>
2023-12-23 22:59:12 -0600

    day 23 faster

---
+++ 23/d.spec.ts
@@ -0,0 +1,14 @@
+import { aocTest, getDayAndPart } from "../aocTester";
+import { solution } from "./d";
+import exampleInput from "./example.input.txt";
+import exampleOutput from "./b.example.output.txt";
+import realInput from "./input.txt";
+import { it } from "bun:test";
+
+const { day, part } = getDayAndPart(import.meta.url);
+
+if (!isNaN(day)) {
+  it(`should work for day ${day} part ${part}'s example`, () => {
+    aocTest(solution, exampleInput, exampleOutput, realInput, day, part);
+  });
+}
